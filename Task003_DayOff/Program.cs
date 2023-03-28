4768a66060769c774e061754ad7d90d791713233
Basinger <fel_simon@mail.ru>
2023-03-29 01:42:46 +0300

    Added Task003_DayOff

---
+++ Task003_DayOff/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+System.Console.Write("Enter the number of a day: ");
+int day = int.Parse(Console.ReadLine());
+DayOff();
+
+void DayOff()
+{
+    if(day == 1 || day == 2 || day == 3 || day == 4 || day == 5)
+    {
+        System.Console.WriteLine("Not day off");
+    }
+    else if (day < 0)   
+    {
+        System.Console.WriteLine("Error! Enter the number of a day");
+    }
+    else if (day == 6 || day == 7)   
+    {
+        System.Console.WriteLine("It's a day off!");
+    }
+    else   
+    {
+        System.Console.WriteLine("Error! Enter the number of a day");
+    }
+}   

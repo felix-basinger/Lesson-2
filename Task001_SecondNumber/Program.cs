b13d1853257acfcc87f20a5a6e5b30fea547548f
Basinger <fel_simon@mail.ru>
2023-03-28 20:04:55 +0300

    Added Tasl001_SecondNumber

---
+++ Task001_SecondNumber/Program.cs
@@ -0,0 +1,54 @@
+﻿// System.Console.Write("Enter three-digit number: ");
+// int i = EnterNumber();
+// System.Console.WriteLine($"Your second digit is: {FindSecondDigit(i)}");
+
+// int EnterNumber()
+// {
+//     return int.Parse(System.Console.ReadLine());
+// }
+
+// int FindSecondDigit(int i)
+// {
+//     if (i > 999 || i < 100)
+//     {
+//         while(i > 999 || i < 100)
+//         {
+//             System.Console.WriteLine("Error! It's not three digit number");
+//             return EnterNumber();
+//         }
+//     }
+//     else
+//     {
+//         int FirstDiv = i % 100;
+//         int SecondDiv = FirstDiv / 10;
+//         return SecondDiv;
+//     }
+// }
+
+
+
+System.Console.Write("Enter three-digit number: ");
+int i = EnterNumber();
+Check();
+System.Console.WriteLine($"Your second digit is: {FindSecondDigit(i)}");
+
+int EnterNumber()
+{
+    return int.Parse(System.Console.ReadLine());
+}
+  
+void Check()
+{
+    while (i > 999 || i < 100)
+    {
+        System.Console.WriteLine("Error! It's not three digit number");
+        EnterNumber();
+    }
+}
+
+int FindSecondDigit(int i)
+{
+    int FirstDiv = i % 100;
+    int SecondDiv = FirstDiv / 10;
+    return SecondDiv;
+}

cc250a56a26b1279f5dce199479703270b59f7d0
Basinger <fel_simon@mail.ru>
2023-03-29 00:56:33 +0300

    Added Task002_ThirdNumber

---
+++ Task002_ThirdNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+System.Console.Write("Enter a number: ");
+int i = int.Parse(Console.ReadLine());
+string str = i.ToString();
+ThirdDigit();
+
+void ThirdDigit()
+{   
+    int length = str.Length;
+    if(length > 2) System.Console.WriteLine($"Your digit is {str[2]}");
+    else System.Console.WriteLine("Error");
+}
+
+ 

b9533a55a135a18f7d5eb413265c035edbedf143
Basinger <fel_simon@mail.ru>
2023-03-29 02:29:06 +0300

    Added first changes in Task001

--- Task001_SecondNumber/Program.cs
+++ Task001_SecondNumber/Program.cs
@@ -1,31 +1,4 @@
-﻿// System.Console.Write("Enter three-digit number: ");
-// int i = EnterNumber();
-// System.Console.WriteLine($"Your second digit is: {FindSecondDigit(i)}");
-
-// int EnterNumber()
-// {
-//     return int.Parse(System.Console.ReadLine());
-// }
-
-// int FindSecondDigit(int i)
-// {
-//     if (i > 999 || i < 100)
-//     {
-//         while(i > 999 || i < 100)
-//         {
-//             System.Console.WriteLine("Error! It's not three digit number");
-//             return EnterNumber();
-//         }
-//     }
-//     else
-//     {
-//         int FirstDiv = i % 100;
-//         int SecondDiv = FirstDiv / 10;
-//         return SecondDiv;
-//     }
-// }
-
-
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 System.Console.Write("Enter three-digit number: ");
 int i = EnterNumber();
@@ -33,21 +6,23 @@ Check();
 System.Console.WriteLine($"Your second digit is: {FindSecondDigit(i)}");
 
 int EnterNumber()
-{
+{   
+    
     return int.Parse(System.Console.ReadLine());
+
 }
   
-void Check()
+bool Check()
 {
-    while (i > 999 || i < 100)
+    if (i > 999 || i < 100)
     {
         System.Console.WriteLine("Error! It's not three digit number");
-        EnterNumber();
-    }
+    } 
+    return false;
 }
 
 int FindSecondDigit(int i)
-{
+{    
     int FirstDiv = i % 100;
     int SecondDiv = FirstDiv / 10;
     return SecondDiv;
